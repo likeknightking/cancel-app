da372c90ba0e4de276052b417020cdb57df3bd99
likeknightking <likeknightking@gmail.com>
2025-01-08 01:34:09 -0300

    Primeiro commit

---
+++ lib/utils.ts
@@ -0,0 +1,22 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(input: string | number): string {
+  const date = new Date(input)
+  return date.toLocaleDateString("pt-BR", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  })
+}
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(amount)
+}
